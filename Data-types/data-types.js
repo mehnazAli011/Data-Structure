71984d45e5bf1c3c8a11a76857b1bc3af0b06f89
mehnaz <mehnazm406@gmail.com>
2024-08-28 14:55:03 +0530

    Update data-types.js

--- Data-types/data-types.js
+++ Data-types/data-types.js
@@ -1,10 +1,30 @@
 //                                  Data Types In JS
+// Data types in Javascript=> Data types in js describe different data or kind of types of data you will be work with and storing  in varibles.understanding and correctly using these data types is cirucal for writing effectiy code.If  data is does not sorted in proper format , it can lead  to unexpected result and error to your code.
+
+
+
+//explain with example to better understanding :-
+//Without Data types
+let age1 = "11";//this is a string ,not a Number.
+age1 = age1+1;//  add the number 1(numricle addtions)
+
+console.log(age1);//"111" 
+//this is a givene   unexpected result because javascript convert 1 to  string=>"11"+"1"(number become string) ,output is "111".
+
+//With Data types
+
+let age2 = 11;//this is number.
+age2=age2+1;// add a number 1(numricle addtions)
+
+console.log(age2);//12 That is a correct output.
+
+//
 
 //                                Data-Types
 //                                    |
 //                       _____________|_______________  
 //                       |                            |
-//                      Primitve Data type            Non-primitve Data type
+//                      Primitve Data types            Non-primitve Data types
 //                       |                              |
 //                       Number                        Object
 //                       String                        Array
